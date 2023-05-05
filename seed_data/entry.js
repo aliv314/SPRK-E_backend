076280507d693724e71f678de3d531c251938520
alv-314 <alicov314@gmail.com>
2023-05-05 02:45:13 -0400

    minor format to seed data

--- seed_data/entry.js
+++ seed_data/entry.js
@@ -1,47 +1,47 @@
 module.exports = [
   {
     id: 1,
-    entry: "Started my day with a refreshing jog in the park. Feeling energized and ready to tackle the day ahead.",
     user_id: 1,
+    entry: "Started my day with a refreshing jog in the park. Feeling energized and ready to tackle the day ahead.",
   },
   {
     id: 2,
-    entry: "Had a great meeting with my team today. We were able to make some important decisions and move our project forward.",
     user_id: 1,
+    entry: "Had a great meeting with my team today. We were able to make some important decisions and move our project forward.",
   },
   {
     id: 3,
-    entry: "Feeling a bit under the weather today. Took some medicine and hoping to feel better soon.",
     user_id: 1,
+    entry: "Feeling a bit under the weather today. Took some medicine and hoping to feel better soon.",
   },
   {
     id: 4,
-    entry: "Had a nice dinner with friends tonight. It's always great to catch up with old friends.",
     user_id: 2,
+    entry: "Had a nice dinner with friends tonight. It's always great to catch up with old friends.",
   },
   {
     id: 5,
-    entry: "Received some disappointing news today. Trying to stay positive and find a solution to the problem.",
     user_id: 2,
+    entry: "Received some disappointing news today. Trying to stay positive and find a solution to the problem.",
   },
   {
     id: 6,
-    entry: "Took a much needed mental health day today. Spent the day reading and relaxing.",
     user_id: 2,
+    entry: "Took a much needed mental health day today. Spent the day reading and relaxing.",
   },
   {
     id: 7,
-    entry: "Feeling grateful for the support of my family and friends during a difficult time.",
     user_id: 3,
+    entry: "Feeling grateful for the support of my family and friends during a difficult time.",
   },
   {
     id: 8,
-    entry: "Had a fun night out with some coworkers tonight. It's always great to let loose and have some fun.",
     user_id: 3,
+    entry: "Had a fun night out with some coworkers tonight. It's always great to let loose and have some fun.",
   },
   {
     id: 9,
-    entry: "Received some good news today. Feeling grateful and optimistic about the future.",
     user_id: 3,
+    entry: "Received some good news today. Feeling grateful and optimistic about the future.",
   },
 ];
