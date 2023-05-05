74e0f51a3b8b15446fbf72ded1429bfa4a80fddb
alv-314 <alicov314@gmail.com>
2023-05-05 00:56:07 -0400

    changes to user seed

--- seeds/userSeed.js
+++ seeds/userSeed.js
@@ -2,51 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-//  exports.up = function (knex) {
-//     return knex.schema.createTable('users', (table) => {
-//       table.uuid('id').primary();
-//       table
-//         .uuid('user_id')
-//         .references('user.id')
-//         .onUpdate('CASCADE')
-//         .onDelete('CASCADE');
-//       table.string('user_name').notNullable();
-//       table.string('user-password').notNullable();
-      
-//     });
-//   };
-  
-//   /**
-//    * @param { import("knex").Knex } knex
-//    * @returns { Promise<void> }
-//    */
-//   exports.down = function (knex) {
-//     return knex.schema.dropTable('users');
-//   };
-  
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
  exports.seed = async function (knex) {
     // Deletes ALL existing entries
-    await knex('users').del();
-    await knex('users').insert([
+    await knex('user').del();
+    await knex('user').insert([
       {
-        user_id: '1',
+        id: '1',
         username: 'Deboeasy',
         password: '123456',
       },
 
       {
-        user_id: '2',
+        id: '2',
         username: 'AlejandroLoco',
         password: '456012',
       },
 
       {
-        user_id: '3',
+        id: '3',
         username: 'GIGI',
         password: '123456',
       }
