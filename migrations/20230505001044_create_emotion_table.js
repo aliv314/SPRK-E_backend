a0425b2459f7f4f08079f9f75351e8c6b29d6b21
alv-314 <alicov314@gmail.com>
2023-05-04 23:25:56 -0400

    added scripts and finished migrate

--- migrations/20230505001044_create_emotion_table.js
+++ migrations/20230505001044_create_emotion_table.js
@@ -3,7 +3,31 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('emotion', (table) => {
+    table.increments('id').primary();
+    table.integer('user_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('user')
+    .onUpdate('CASCADE')
+    .onDelete('CASCADE');
+    table.timestamp('timestamp').defaultTo(knex.fn.now());
+    table.float('positive').notNullable();
+    table.float('negative').notNullable();
+    table.float('anger').notNullable();
+    table.float('frustration').notNullable();
+    table.float('fear').notNullable();
+    table.float('confusion').notNullable();
+    table.float('disappointment').notNullable();
+    table.float('confidence').notNullable();
+    table.float('joy').notNullable();
+    table.float('satisfaction').notNullable();
+    table.float('compassion').notNullable();
+    table.float('enthusiasm').notNullable();
+    table.float('thoughtful').notNullable();
+  })
 };
 
 /**
@@ -11,5 +35,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('emotion')
 };
