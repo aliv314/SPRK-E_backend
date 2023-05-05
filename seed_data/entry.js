84ece91ddcb9b20ba1dcae9eaf0b21029749c08e
alv-314 <alicov314@gmail.com>
2023-05-05 02:34:04 -0400

    emotion seed data

--- seed_data/entry.js
+++ seed_data/entry.js
@@ -1,64 +1,55 @@
 module.exports = [
   {
     id: 1,
-    entry:
-      "Started my day with a refreshing jog in the park. Feeling energized and ready to tackle the day ahead.",
+    entry: "Started my day with a refreshing jog in the park. Feeling energized and ready to tackle the day ahead.",
     user_id: 1,
     timestamp: 1657526400000,
   },
   {
     id: 2,
-    entry:
-      "Had a great meeting with my team today. We were able to make some important decisions and move our project forward.",
+    entry: "Had a great meeting with my team today. We were able to make some important decisions and move our project forward.",
     user_id: 1,
     timestamp: 1657765420000,
   },
   {
     id: 3,
-    entry:
-      "Feeling a bit under the weather today. Took some medicine and hoping to feel better soon.",
+    entry: "Feeling a bit under the weather today. Took some medicine and hoping to feel better soon.",
     user_id: 1,
     timestamp: 1658002200000,
   },
   {
     id: 4,
-    entry:
-      "Had a nice dinner with friends tonight. It's always great to catch up with old friends.",
+    entry: "Had a nice dinner with friends tonight. It's always great to catch up with old friends.",
     user_id: 2,
     timestamp: 1658261100000,
   },
   {
     id: 5,
-    entry:
-      "Received some disappointing news today. Trying to stay positive and find a solution to the problem.",
+    entry: "Received some disappointing news today. Trying to stay positive and find a solution to the problem.",
     user_id: 2,
     timestamp: 1658530300000,
   },
   {
     id: 6,
-    entry:
-      "Took a much needed mental health day today. Spent the day reading and relaxing.",
+    entry: "Took a much needed mental health day today. Spent the day reading and relaxing.",
     user_id: 2,
     timestamp: 1658769600000,
   },
   {
     id: 7,
-    entry:
-      "Feeling grateful for the support of my family and friends during a difficult time.",
+    entry: "Feeling grateful for the support of my family and friends during a difficult time.",
     user_id: 3,
     timestamp: 1659028800000,
   },
   {
     id: 8,
-    entry:
-      "Had a fun night out with some coworkers tonight. It's always great to let loose and have some fun.",
+    entry: "Had a fun night out with some coworkers tonight. It's always great to let loose and have some fun.",
     user_id: 3,
     timestamp: 1721683200000,
   },
   {
     id: 9,
-    entry:
-      "Received some good news today. Feeling grateful and optimistic about the future.",
+    entry: "Received some good news today. Feeling grateful and optimistic about the future.",
     user_id: 3,
     timestamp: 1721946000000,
   },
